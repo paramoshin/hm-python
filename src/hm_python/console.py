890b2a9efbd4cac33e423d67fa36322179566894
Anton Paramoshin <a.paramoshin@lcgroup.su>
2020-08-24 18:38:39 +0700

    feat: implement chapter 2

--- src/hm_python/console.py
+++ src/hm_python/console.py
@@ -1,21 +1,23 @@
 import textwrap
 
 import click
-import requests
 
-from . import __version__
-
-
-API_URL = "https://ru.wikipedia.org/api/rest_v1/page/random/summary"
+from . import __version__, wikipedia
 
 
 @click.command()
+@click.option(
+    "--language",
+    "-l",
+    default="ru",
+    help="Language edition of Wikipedia",
+    metavar="LANG",
+    show_default=True,
+)
 @click.version_option(version=__version__)
-def main():
+def main(language):
     """The hypermodern Python project."""
-    with requests.get(API_URL) as response:
-        response.raise_for_status()
-        data = response.json()
+    data = wikipedia.random_page(lang=language)
 
     title = data["title"]
     extract = data["extract"]
---
+++ src/hm_python/wikipedia.py
@@ -0,0 +1,18 @@
+import click
+import requests
+
+
+API_URL = "https://{lang}.wikipedia.org/api/rest_v1/page/random/summary"
+
+
+def random_page(lang="ru"):
+    url = API_URL.format(lang=lang)
+
+    try:
+        with requests.get(url) as response:
+            response.raise_for_status()
+            return response.json()
+    except requests.RequestException as e:
+        message = str(e)
+        raise click.ClickException(message)
+
